5cbad2acc42bedcc658ba0554d91591d53e312d7
Melissa Van Bussel <melissavanbussel@trentu.ca>
2024-01-11 20:02:37 -0500

    Add code from this YouTube tutorial: https://youtu.be/8tilyqp4bZY

---
+++ httr2/httr2_examples.R
@@ -0,0 +1,54 @@
+# Load packages
+library(httr2)
+library(tidyverse)
+
+# Define access tokens
+access_token_openai <- "YOUR KEY HERE"
+access_token_gitlab <- "YOUR KEY HERE"
+
+# Define base URLs
+base_url_openai <- "https://api.openai.com/v1"
+base_url_gitlab <- "https://gitlab.com/api/v4"
+
+# Define endpoints
+endpoint_openai <- "/images/generations"
+endpoint_gitlab <- "/projects/53405032/issues"
+
+# Define request
+req_openai <- request(paste0(base_url_openai, endpoint_openai))
+req_gitlab <- request(paste0(base_url_gitlab, endpoint_gitlab))
+
+# Provide access token via header
+headers_openai <- req_openai %>%
+  req_headers(
+    `Content-Type` = "application/json",
+    `Authorization` = paste0("Bearer ", access_token_openai)
+  )
+headers_gitlab <- req_gitlab %>%
+  req_headers(
+    `PRIVATE-TOKEN` = access_token_gitlab
+  )
+
+# Create response body
+resp_body_openai <- headers_openai %>%
+  req_body_json(
+    list(prompt = "A cute baby sea otter")
+  )
+
+# Perform response 
+req_perform_openai <- resp_body_openai %>% 
+  req_perform()
+req_perform_gitlab <- headers_gitlab %>%
+  req_perform()
+
+# Extract results from the response
+results_openai <- req_perform_openai %>%
+  resp_body_json()
+results_gitlab <- req_perform_gitlab %>%
+  resp_body_json()
+
+# Explore results
+results_openai$data[[1]]$url
+results_gitlab[[1]]$milestone$title
+results_gitlab[[1]]$title
+results_gitlab[[1]]$time_stats$total_time_spent
